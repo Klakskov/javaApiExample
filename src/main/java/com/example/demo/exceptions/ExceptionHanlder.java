9c04f10c13a3e7816174e9ae6531dfcf56a7e443
Cristante <thales.cristante@accenture.com>
2023-06-04 21:08:22 -0300

    addition of field error into response body

--- src/main/java/com/example/demo/exceptions/ExceptionHanlder.java
+++ src/main/java/com/example/demo/exceptions/ExceptionHanlder.java
@@ -1,13 +1,13 @@
 package com.example.demo.exceptions;
 
-import com.example.demo.exceptions.model.BusinessException;
-import com.example.demo.exceptions.model.Problem;
-import com.example.demo.exceptions.model.ProblemType;
-import com.example.demo.exceptions.model.ValidationRequestException;
+import com.example.demo.exceptions.model.*;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -15,6 +15,8 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import javax.validation.ValidationException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class ExceptionHanlder extends ResponseEntityExceptionHandler {
@@ -23,9 +25,23 @@ public class ExceptionHanlder extends ResponseEntityExceptionHandler {
     private final HttpStatus httpStatusValidationException =  HttpStatus.BAD_REQUEST;
 
     @Override
-    protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status,
-                                                         WebRequest request) {
-        return super.handleExceptionInternal(ex, ex.getBindingResult(), headers, status, request);
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        List<String> errors = new ArrayList<String>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.add(error.getField() + ": " + error.getDefaultMessage());
+        }
+        for (ObjectError error : ex.getBindingResult().getGlobalErrors()) {
+            errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
+        }
+
+        ApiError apiError =
+                new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), errors);
+        return handleExceptionInternal(
+                ex, apiError, headers, apiError.getStatus(), request);
     }
 
     @ExceptionHandler(BusinessException.class)
